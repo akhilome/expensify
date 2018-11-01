c3668ec10d64812485ba5ff2994aad46882f7134
Kizito Akhilome <hovkard@gmail.com>
2018-11-01 17:12:01 +0100

    install & setup redux learn about ...
    - what a redux store is
    - setting up initial state
    - dispactching actions to modify state

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const { join } = require('path');
 
 module.exports = {
-  entry: './src/app.jsx',
+  entry: './playground/redux-101.js',
   output: {
     path: join(__dirname, 'public'),
     filename: 'bundle.js'
