d57bcc60c4ec77cbbcd04a31956bd25bbc633890
Kizito Akhilome <hovkard@gmail.com>
2018-11-10 19:43:44 +0100

    make the necessary imports from app root & test for working state

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+store.dispatch(addExpense({ description: 'Water Bill' }));
+store.dispatch(addExpense({ description: 'Gas Bill' }));
+store.dispatch(setTextFilter('water'));
+
+console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
