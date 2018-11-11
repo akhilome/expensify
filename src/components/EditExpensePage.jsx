9d0d6bce0d20fb561fba97d00706d20542425f2f
Kizito Akhilome <hovkard@gmail.com>
2018-11-11 20:30:05 +0100

    implement ability for user to actually edit an expense

--- src/components/EditExpensePage.jsx
+++ src/components/EditExpensePage.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense } from '../actions/expenses';
 
 const EditExpensePage = (props) => (
   <div>
-    <p>This is from my Edit Expense {props.match.params.id} page!</p>
+    <ExpenseForm
+      expense={props.expense}
+      onSubmit={(expense) => {
+        dispatch(editExpense(id, expense));
+        props.history.push('/');
+      }}
+    />
+    <button
+      onClick={() => {
+        props.dispatch(removeExpense({ id: props.expense.id }));
+        props.history.push('/');
+      }}
+    >Remove</button>
   </div>
 );
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+  return { expense: state.expenses.find(expense => expense.id === props.match.params.id) }
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
