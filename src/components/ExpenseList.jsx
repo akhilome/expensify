5cbe13b84e95d783e06d53f8f93b29201cfb848d
Kizito Akhilome <hovkard@gmail.com>
2018-11-11 07:44:12 +0100

    render individual expenses to page

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
+import selectExpenses from '../selectors/expenses';
 
 const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
-    {props.expenses.map(expense => <p key={expense.description}>{expense.description}</p>)}
+    {props.expenses.map((expense) => {
+      return <ExpenseListItem
+        { ...expense }
+        key={expense.id}
+      />
+    })}
   </div>
 );
 
-const mapStateToProps = (state) => {
-  return {
-    expenses: state.expenses,
-    filters: state.filters
-  };
-};
+const mapStateToProps = (state) => ({expenses: selectExpenses(state.expenses, state.filters)});
 
 export default connect(mapStateToProps)(ExpenseList);
