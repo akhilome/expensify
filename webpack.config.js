1343570e72f3e81c68ebf9768db2e481e2938702
Kizito Akhilome <hovkard@gmail.com>
2018-11-20 11:59:05 +0100

    get ready for deployment: minimal webpack prod setup
    - add build scripts to build for various environments (production & otherwise)

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,35 @@
 const { join } = require('path');
 
-module.exports = {
-  entry: './src/app.jsx',
-  output: {
-    path: join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+    entry: './src/app.jsx',
+    output: {
+      path: join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
 };
