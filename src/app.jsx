56a42b8109924180579f166f433e213d51f8ad03
Kizito Akhilome <hovkard@gmail.com>
2018-11-20 13:57:18 +0100

    remove placeholder demo expenses

--- src/app.jsx
+++ src/app.jsx
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
 import 'normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-store.dispatch(addExpense({ description: 'Water Bill', amount: 4500, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 1009030 }));
-store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 500 }));
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
